252cf682cfe4e81dd7470e5cd49a3e0399b5fb68
hassehulabeck <hansbudandersson@gmail.com>
2020-05-27 19:08:22 +0200

    After wednesday afternoon

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('products')->insert([
+            'name' => Str::random(20),
+            'price' => '163.23',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,7 @@
 @extends ('main')
 @section('main')
 @parent
-<p>Innehåll från index.blade.php</p>
-@endsection
-
-@section('info')
-
-<form action="/input" method="post">
-    @csrf
-    @method ('PUT')
-    <input type="text" name="username">
-    <input type="submit" value="Gå till input" name="goToInput">
-</form>
-
+@foreach($products as $product)
+<p> {{ $product->name}} {{ $product->price }}</p>
+@endforeach
 @endsection
